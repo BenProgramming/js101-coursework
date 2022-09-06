5730c32b512dc3fd96d4ba18bab7e1f21a620f84
Ben Lane <lanebenjamin192@gmail.com>
2022-09-06 16:12:09 -0500

    added function for change in language

--- lesson-2/refactor-calulator.js
+++ lesson-2/refactor-calulator.js
@@ -1,6 +1,12 @@
 // A calculator that ask the User to numbers to sum
 
 const rlSync = require('readline-sync');
+const MSG_FILE = require('./re-calc-messages.json');
+const LANGUAGE = 'en';
+
+function message(message, lang='en') {
+  return MSG_FILE[lang][message];
+}
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -10,48 +16,57 @@ function invalidNumber(number) {
   return Number.isNaN(number) || number.trimStart() === '';
 }
 
-prompt('Welcome to Calculator!');
+prompt(message("wlcm", LANGUAGE));
 
-prompt("What's your first number?");
-let number1 = rlSync.question();
+function input() {
+  prompt(message("frstNumQ", LANGUAGE));
+  let number1 = rlSync.question();
 
-while (invalidNumber(number1)) {
-  prompt("Hmm that doesn't look right... Please enter a valid number: ");
-  number1 = rlSync.question();
+  while (invalidNumber(number1)) {
+    prompt(message("wrgNum", LANGUAGE));
+    number1 = rlSync.question();
+  }
+
+  prompt(message("secNumQ", LANGUAGE));
+  let number2 = rlSync.question();
+
+  while (invalidNumber(number2)) {
+    prompt(message("wrgNum", LANGUAGE));
+    number2 = rlSync.question();
+  }
+
+  prompt(`${message("numPrompt1", LANGUAGE)} ${number1} ${message("numPrompt2", LANGUAGE)} ${number2}`);
+
+  prompt(message("operListQ", LANGUAGE));
+  let operation = rlSync.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(message("wrgOpNum", LANGUAGE));
+    operation = rlSync.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${message("resultPrompt", LANGUAGE)} ${output} `);
 }
 
-prompt("What's your second number?");
-let number2 = rlSync.question();
-
-while (invalidNumber(number2)) {
-  prompt("Hmm that doesn't look right... Please enter a valid number: ");
-  number2 = rlSync.question();
-}
-
-prompt(`number1: ${number1} number2: ${number2}`);
-
-prompt('What operation would you like to perform on these two numbers? \n1) Add\n2) Substract\n3) Multiply \n4) Divide');
-let operation = rlSync.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("You must choose 1, 2, 3, or 4");
-  operation = rlSync.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-prompt(`The result is: ${output} `);
+let another;
+do {
+  input(); 
+  prompt(message("anotherCalc", LANGUAGE));
+  another = rlSync.question();
+} while (another === "1");
