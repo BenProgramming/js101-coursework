86c3e3b3eda7e55d5763ea92818dee8a09154e24
Ben Lane <lanebenjamin192@gmail.com>
2022-10-05 16:50:13 -0500

    added instructions, abstracted body to helper functions

--- lesson-2/rps-bonus-feat/rps-bonus.js
+++ lesson-2/rps-bonus-feat/rps-bonus.js
@@ -8,6 +8,7 @@ const WINNING_COMBINATIONS = {
   lizard: ['paper', 'spock'],
   spock: ['rock', 'scissors']
 };
+const WIN_SCORE = 3;
 
 function playerWins(usrCh, cpuCh) {
   return WINNING_COMBINATIONS[usrCh].includes(cpuCh);
@@ -21,11 +22,16 @@ function input(message) {
   return readline.question(`=> ${message}`);
 }
 
+function computerChoice() {
+  let randomIndex = Math.floor(Math.random() * (VALID_CHOICES.length - 5));
+  return VALID_CHOICES[randomIndex];
+}
+
 while (true) {
   let wins = 0;
   let cpuWins = 0;
 
-  while (!(wins === 3 || cpuWins === 3)) {
+  while (!(wins === WIN_SCORE|| cpuWins === WIN_SCORE)) {
     let choice = input(`Choose one: ${VALID_CHOICES.join(', ')}: `);
     while (!VALID_CHOICES.includes(choice)) {
       choice = input('Please enter a valid choice: ');
@@ -33,10 +39,8 @@ while (true) {
     if (VALID_CHOICES.indexOf(choice) > 4) {
       choice = VALID_CHOICES[VALID_CHOICES.indexOf(choice) - 5];
     }
-
-    let randomIndex = Math.floor(Math.random() * (VALID_CHOICES.length - 5));
-    let rpsCpuChoice = VALID_CHOICES[randomIndex];
-
+    
+    let rpsCpuChoice = computerChoice();
     output(`You chose ${choice}, the 'CPU' chose ${rpsCpuChoice}`);
 
     if (rpsCpuChoice === choice) {
@@ -56,10 +60,24 @@ while (true) {
     output(`You are the Grand Winner!`);
   }
 
-  let contQ = input(`Would you like to play again (y/n)?\n`).toLowerCase();
-  while (contQ[0] !== 'n' && contQ[0] !== 'y') {
-    contQ = input(`Please enter 'y' or 'n'\n`).toLowerCase();
+  let contQ = input(`Would you like to play again (y/n)?\n`).toLowerCase()
+  .trim();
+  console.log("The value of contQ:\n", contQ);
+  while (contQ !== 'n' && contQ !== 'y' && contQ !== 'yes' && contQ !== 'no') {
+    contQ = input(`Please enter 'y', 'n', or 'yes', 'no' to continue\n`)
+    .toLowerCase();
   }
 
-  if (contQ[0] === 'n') break;
+  if (contQ === 'n' || contQ === 'no') break;
 }
+
+
+/* REVIEW NOTES 
+  - Welcome message explaining how Lizard and Spock work
+  - yesno working as a yes => check for the explicit values of 'y' 'n' and 'yes'
+    and 'no'
+  - const variable for the number of rounds + adding a function for User to 
+    select the number of rounds that they would like to play
+  - Add current score to the message after round
+
+*/
